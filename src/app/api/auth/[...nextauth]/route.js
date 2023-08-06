48a58859152f9bb45fa0abc8121bcc91989ca01d
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-08-06 18:46:50 -0500

    Fixing prisma issues

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,11 +2,11 @@
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import prisma from "../../../../../lib/prisma";
-import { PrismaAdapter } from "@prisma/client";
+// import prisma from "../../../../../lib/prisma";
+// import { PrismaAdapter } from "@prisma/client";
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
