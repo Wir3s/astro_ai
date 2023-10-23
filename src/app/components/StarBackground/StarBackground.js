53ee2eb7c04dc4dbb8dd7334bd578aefd33cbc3e
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-10-22 21:07:45 -0500

    Two different stars rendering

--- src/app/components/StarBackground/StarBackground.js
+++ src/app/components/StarBackground/StarBackground.js
@@ -3,7 +3,9 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../../page.module.css";
-import starImage from "../../../../public/images/lens_flare.png";
+import starImage1 from "../../../../public/images/lens_flare.png";
+import starImage2 from "../../../../public/images/lens_flare_white2.png";
+
 // import starImage from './lens_flare.png';
 
 export default function StarBackground() {
@@ -16,7 +18,9 @@ export default function StarBackground() {
       const size = Math.floor(Math.random() * (50 - 10 + 1)) + 10;
       const x = Math.floor(Math.random() * window.innerWidth);
       const y = Math.floor(Math.random() * window.innerHeight);
-      newStars.push({ size, x, y });
+      const duration = Math.random() * (15 - 5) + 1; // Animation duration between 5 and 15 seconds
+      const image = Math.random() > 0.5 ? starImage1 : starImage2; // Randomly select an image
+      newStars.push({ size, x, y, duration, image });
     }
     setStars(newStars);
   }, []);
@@ -43,15 +47,16 @@ export default function StarBackground() {
             // backgroundSize: "cover",
             left: `${star.x}px`,
             top: `${star.y}px`,
+            animationDuration: `${star.duration}s`,
           }}
         >
-        <Image
-        src={starImage}  // Using the imported image
-        alt="Star"  // Alt text for accessibility
-        width={star.size}  // Dynamic sizing
-        height={star.size}
-      />
-    </div>
+          <Image
+            src={star.image} // Use the image assigned to the star
+            alt="Star" // Alt text for accessibility
+            width={star.size} // Dynamic sizing
+            height={star.size}
+          />
+        </div>
       ))}
     </div>
   );
