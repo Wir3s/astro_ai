f4a4d6c9ebb76aed53a4ca63617fcfa7e5ed763f
Wir3s <113479857+Wir3s@users.noreply.github.com>
2024-02-16 20:52:44 -0600

    password added to User, new user registration route working

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,32 +1,26 @@
 import prisma from "../../../../lib/prisma";
-import bcrypt from 'bcrypt';
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
-export default async function register(req, res) {
-  const { name, email, password } = req.body;
-
-  try {
-    const exists = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (exists) {
-      return res.status(409).json({ error: "User already exists" }); // 409 Conflict
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-
+export async function POST(req, res) {
+  const { name, email, password } = await req.json();
+  const exists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (exists) {
+    return NextResponse.json({ error: "User already exists" });
+  } else {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
       data: {
         name,
         email,
-        password: hashedPassword, // Store the hashed password
+        password: hashedPassword,
       },
     });
 
-    res.status(201).json(user); // 201 Created
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    return NextResponse.json(user);
   }
 }
-
