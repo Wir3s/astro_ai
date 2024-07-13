92ee15bf2459b56da0376b17e12d220134e0fcbb
Wir3s <113479857+Wir3s@users.noreply.github.com>
2024-07-13 17:10:16 -0500

    Removed signs nav menu option

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -35,7 +35,6 @@ export default function Header() {
             </li>
           )}
           <li>{session ? <LogoutButton /> : <LoginButton />}</li>
-          <Burgs />
         </ul>
       </nav>
     </header>
