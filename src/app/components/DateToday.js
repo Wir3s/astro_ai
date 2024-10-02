a95bfc87fb0edcf6bcf4498209ee3a6eaf63f4ae
Wir3s <113479857+Wir3s@users.noreply.github.com>
2024-10-01 20:13:14 -0500

    Fixed DateToday component for CST timezone

--- src/app/components/DateToday.js
+++ src/app/components/DateToday.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styles from './DateToday.module.css'
+import React from "react";
+import styles from "./DateToday.module.css";
 
 function DateToday() {
   let today = new Date();
 
-  let options = { weekday: 'long', month: 'long', day: 'numeric' };
-  let dateString = today.toLocaleDateString('en-US', options);
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    timeZone: "America/Chicago",
+  };
+  let dateString = today.toLocaleDateString("en-US", options);
 
-  return (
-    <div className={styles.date}>
-      {dateString}
-    </div>
-  );
+  return <div className={styles.date}>{dateString}</div>;
 }
 
 export default DateToday;
